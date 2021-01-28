1372ff32794d2163654bbfa35243cb0cfb2e278a
star0angel@qq.com <star0angel@qq.com>
2021-01-28 18:32:54 +0800

    添加项目文件。

---
+++ main.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:4996)//取消scanf返回值的警告
+
+#include <stdio.h>
+//去除输入中的多个空格置输出一个
+int main()
+{
+	int c;
+	char d='a';
+	while ((c = getchar()) != EOF)
+	{
+		if (c!=' ')
+		{
+			putchar(c);
+		}
+		else
+		{
+			if (d != ' ')
+			{
+				putchar(c);
+
+			}
+			
+			
+		}
+		d = c;
+		
+	}
+}
